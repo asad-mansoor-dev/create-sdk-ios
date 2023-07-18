e2a9da9c5eb28d55217a1e293d34e8ad803ffd84
Asad Mehmood <asadpk80@gmail.com>
2023-07-18 06:23:24 -0400

    Framework functionality class added
    This loads web view for a fixed URL

---
+++ Sources/MediumSDK/MediumWebView.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dev on 7/18/23.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+public class MediumWebView: WKWebView {
+    
+    public init() {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = true
+        super.init(frame: .zero, configuration: webConfiguration)
+        self.scrollView.isScrollEnabled = false
+        self.isMultipleTouchEnabled = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    @discardableResult
+    public func load() -> WKNavigation? {
+        if let url = URL(string:"http://example.com") {
+            let req = URLRequest(url: url)
+            return super.load(req)
+        }
+        return nil
+    }
+}
